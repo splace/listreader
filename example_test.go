5155be8e4ac16e2f814a0a3462263fc6f6a55853
simon <psiplace@netscape.net>
2017-09-10 17:23:54 +0100

    example

---
+++ example_test.go
@@ -0,0 +1,28 @@
+package listreader_test
+
+import "github.com/splace/listreader"
+
+import "strings"
+import "fmt"
+
+func ExampleCSV() {
+	source:=strings.NewReader("1,2,3\n4,5,6,7")
+	lineReader := &listreader.PartReader{Reader:source, Delimiter:'\n'}
+	for {
+		floatReader := listreader.NewFloats(lineReader, ',')
+		fs,err:=floatReader.ReadAll()
+		if err != nil {
+				fmt.Println(err)
+				break
+		}
+		fmt.Println(lineReader.Count,fs)
+		err = lineReader.Next()
+		if err != nil {
+				break
+		}
+	}
+	// Output:
+	// 0 [1 2 3]
+	// 1 [4 5 6 7]
+}
+
